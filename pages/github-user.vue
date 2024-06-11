eab16e2c537690608a46ca7ddf2a2a26553e790a
Gunawan <gnnawan.dev@gmail.com>
2024-06-12 05:43:11 +0700

    Fetching Data with useFetch

---
+++ pages/github-user.vue
@@ -0,0 +1,9 @@
+<script setup>
+const { data: user } = useFetch("https://api.github.com/users/gunawan-ctrl");
+</script>
+
+<template>
+  <h1>{{ user?.name }}</h1>
+  <h1>{{ user?.location }}</h1>
+  <p>{{ user?.following }}</p>
+</template>
---
+++ pages/github.vue
@@ -0,0 +1,12 @@
+<script setup>
+const { data: users } = useFetch("https://api.github.com/users");
+</script>
+
+<template>
+  <h1>Github users</h1>
+  <div v-for="user in users" :key="user.id">
+    <h2>{{ user.login }}</h2>
+    <p>name: {{ user?.name }}</p>
+    <p>id is: {{ user.id }}</p>
+  </div>
+</template>
