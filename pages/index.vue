05d898f46009590afe86e2052a0fae005e28e977
Gunawan <gnnawan.dev@gmail.com>
2024-06-12 08:51:51 +0700

    Server Routes & Using it Our Project

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,13 @@
 useHead({
   title: "Home Page",
 });
+
+const { data } = await useFetch("/api/userdetails");
 </script>
 
 <template>
   <h1>Home Page</h1>
+  <p>
+    {{ data.msg }}
+  </p>
 </template>
